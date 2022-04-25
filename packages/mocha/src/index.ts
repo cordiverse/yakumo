7d86bc3574dba1b6846e19e92b4da4728a7649bc
Shigma <shigma10826@gmail.com>
2022-04-25 23:47:17 +0800

    feat: support yakumo mocha

---
+++ packages/mocha/src/index.ts
@@ -0,0 +1,13 @@
+import { addHook } from 'yakumo'
+import Mocha from 'mocha'
+import globby from 'globby'
+
+addHook('mocha', async ({ targets, cwd }) => {
+  const patterns = Object
+    .keys(targets)
+    .map(folder => `${folder}/tests/*.spec.ts`.slice(1))
+
+  const mocha = new Mocha()
+  mocha.files = await globby(patterns, { cwd })
+  mocha.run(failures => process.exit(failures))
+})
