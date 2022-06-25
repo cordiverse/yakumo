66b0cfd869a86169b1769b55e7aae7d5cba68442
Shigma <shigma10826@gmail.com>
2022-06-26 03:00:06 +0800

    fix(mocha): support empty input

--- packages/mocha/src/index.ts
+++ packages/mocha/src/index.ts
@@ -6,13 +6,13 @@ Error.stackTraceLimit = 50
 
 register('mocha', async (project) => {
   const { cwd, argv } = project
-  const patterns = argv._.flatMap((arg: string) => {
+  const patterns = argv._.length ? argv._.flatMap((arg: string) => {
     const [folder] = arg.split('/', 1)
     const name = arg.slice(folder.length + 1) || '*'
     return project.locate(folder).map((path) => {
       return `${path}/tests/${name}.spec.ts`.slice(1)
     })
-  })
+  }) : ['**/tests/*.spec.ts']
 
   const mocha = new Mocha()
   mocha.files = await globby(patterns, { cwd })
