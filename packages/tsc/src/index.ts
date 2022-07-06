74faa5e110f11e9ae71cb78bc34f96769ff055ec
Shigma <shigma10826@gmail.com>
2022-07-07 01:03:06 +0800

    feat(tsc): support dtsc outFile

--- packages/tsc/src/index.ts
+++ packages/tsc/src/index.ts
@@ -1,11 +1,16 @@
 import { promises as fsp } from 'fs'
+import { join } from 'path'
 import { register, cwd, PackageJson, spawnAsync } from 'yakumo'
+import { build } from 'dtsc'
+import json5 from 'json5'
+import ts from 'typescript'
 
 interface Node {
-  path?: string
-  meta?: PackageJson
-  prev?: string[]
-  next?: Set<string>
+  bundle: boolean
+  path: string
+  meta: PackageJson
+  prev: string[]
+  next: Set<string>
 }
 
 interface Reference {
@@ -15,17 +20,24 @@ interface Reference {
 interface TsConfig {
   files?: string[]
   references?: Reference[]
+  compilerOptions?: ts.CompilerOptions
 }
 
 register('tsc', async (project) => {
+  // Step 1: initialize nodes
   const { targets } = project
   const nodes: Record<string, Node> = {}
   for (const path in targets) {
     const meta = targets[path]
     if (!meta.main) continue
-    nodes[meta.name] = { path, meta, prev: [], next: new Set() }
+    const fullpath = join(cwd, path)
+    const content = await fsp.readFile(fullpath + '/tsconfig.json', 'utf-8')
+    const config: TsConfig = json5.parse(content)
+    const bundle = !!config.compilerOptions?.outFile
+    nodes[meta.name] = { bundle, path, meta, prev: [], next: new Set() }
   }
 
+  // Step 2: build dependency graph
   for (const name in nodes) {
     const { meta } = nodes[name]
     const deps = {
@@ -41,30 +53,60 @@ register('tsc', async (project) => {
     delete nodes[name].meta
   }
 
-  function check(name: string) {
-    const node = nodes[name]
-    if (node.next.size) return true
-    delete nodes[name]
-    config.references.unshift({ path: '.' + node.path })
-    node.prev.forEach(dep => {
-      nodes[dep].next.delete(name)
-    })
+  // Step 3: generate bundle workflow
+  let bundle = false
+  const layers: Node[][] = []
+  while (Object.keys(nodes).length) {
+    const layer: Node[] = []
+    bundle = !bundle
+    let flag = true
+    while (flag) {
+      flag = false
+      for (const name of Object.keys(nodes)) {
+        const node = nodes[name]
+        if (node.next.size || node.bundle === bundle) continue
+        flag = true
+        layer.push(node)
+        delete nodes[name]
+        node.prev.forEach((prev) => {
+          nodes[prev].next.delete(name)
+        })
+      }
+    }
+    if (layers.length && !layer.length) {
+      console.log(nodes)
+      throw new Error('circular dependency detected')
+    }
+    layers.unshift(layer)
   }
 
-  let names: string[]
-  const config: TsConfig = { files: [], references: [] }
-  do {
-    names = Object.keys(nodes)
-  } while (names.length && !names.every(check))
-
-  if (names.length) {
-    console.log(nodes)
-    throw new Error('circular dependency detected')
+  // Step 4: generate dts files
+  // make sure the number of layers is even
+  if (bundle) layers.unshift([])
+  for (let i = 0; i < layers.length; i += 2) {
+    const bundleTargets = layers[i]
+    const buildTargets = layers[i + 1]
+    await Promise.all([
+      prepareBuild(buildTargets),
+      bundleNodes(bundleTargets),
+    ])
+    if (buildTargets.length) {
+      const code = await spawnAsync(['tsc', '-b', 'tsconfig.temp.json'])
+      if (code) process.exit(code)
+    }
   }
-
-  if (!config.references.length) return
-  await fsp.writeFile(cwd + '/tsconfig.temp.json', JSON.stringify(config))
-
-  const code = await spawnAsync(['tsc', '-b', 'tsconfig.temp.json'])
-  if (code) process.exit(code)
 })
+
+async function prepareBuild(nodes: Node[]) {
+  if (!nodes.length) return
+  await fsp.writeFile(cwd + '/tsconfig.temp.json', JSON.stringify({
+    files: [],
+    references: nodes.map(node => ({ path: './' + node.path })),
+  }, null, 2))
+}
+
+async function bundleNodes(nodes: Node[]) {
+  for (const node of nodes) {
+    await build(join(cwd, node.path), ['-p', 'tsconfig.json'])
+  }
+}
