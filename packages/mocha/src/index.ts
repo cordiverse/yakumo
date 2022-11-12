c9b73e387784c754cf738961a27523359d566488
Shigma <shigma10826@gmail.com>
2022-11-12 14:26:22 +0800

    feat(mocha): support mocha options

--- packages/mocha/src/index.ts
+++ packages/mocha/src/index.ts
@@ -1,34 +1,57 @@
 import { register } from 'yakumo'
-import Mocha from 'mocha'
-import globby from 'globby'
+import { spawn } from 'child_process'
+import { loadOptions } from 'mocha/lib/cli/options'
+import { isNodeFlag } from 'mocha/lib/cli/node-flags'
+import unparse from 'yargs-unparser'
 
-Error.stackTraceLimit = 50
+const trimV8Option = value => value !== 'v8-options' && /^v8-/.test(value) ? value.slice(3) : value
 
 register('mocha', async (project) => {
-  const { cwd, argv } = project
-  const patterns = argv._.length ? argv._.flatMap((arg: string) => {
-    const [folder] = arg.split('/', 1)
-    const name = arg.slice(folder.length + 1) || '*'
-    return project.locate(folder).map((path) => {
-      return `${path}/tests/${name}.spec.ts`.slice(1)
+  const opts = loadOptions(process.argv.slice(3))
+  if (opts._.length) {
+    opts._ = opts._.flatMap((arg: string) => {
+      const [folder] = arg.split('/', 1)
+      const name = arg.slice(folder.length + 1) || '*'
+      return project.locate(folder).map((path) => {
+        return `${path}/tests/${name}.spec.ts`.slice(1)
+      })
     })
-  }) : ['**/tests/*.spec.ts']
-
-  for (const filename of argv.require || []) {
-    project.require(filename)
+  } else {
+    opts._ = ['**/tests/*.spec.ts']
   }
 
-  const mocha = new Mocha()
-  mocha.files = await globby(patterns, { cwd, ignore: ['node_modules'] })
-  mocha.run(failures => process.exit(failures))
+  const mochaArgs = {}
+  const nodeArgs = {}
+  Object.keys(opts).forEach(opt => {
+    if (isNodeFlag(opt)) {
+      nodeArgs[trimV8Option(opt)] = opts[opt]
+    } else {
+      mochaArgs[opt] = opts[opt]
+    }
+  })
+
+  const child = spawn(process.execPath, [
+    ...unparse(nodeArgs),
+    require.resolve('mocha/lib/cli/cli'),
+    ...unparse(mochaArgs),
+  ], {
+    stdio: 'inherit',
+    cwd: project.cwd,
+  })
+
+  child.on('exit', (code, signal) => {
+    process.on('exit', () => {
+      if (signal) {
+        process.kill(process.pid, signal)
+      } else {
+        process.exit(code)
+      }
+    })
+  })
+
+  process.on('SIGINT', () => {
+    child.kill('SIGINT')
+  })
 }, {
   manual: true,
-  string: ['require'],
-  array: ['require'],
-  alias: {
-    require: ['r'],
-  },
-  configuration: {
-    'greedy-arrays': false,
-  },
 })
