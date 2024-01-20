1ccb44739b2bfff8fa8bebcbd10e5917199478d5
Shigma <shigma10826@gmail.com>
2024-01-21 02:09:50 +0800

    feat(core): support `--import` cli option

--- packages/core/src/cli.ts
+++ packages/core/src/cli.ts
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
 
-import { main } from '@cordisjs/cli'
+import { start } from '@cordisjs/cli'
 
-main({ name: 'yakumo' })
+const args = process.argv.slice(2)
+
+for (let i = 0; i < args.length; i++) {
+  if (args[i].startsWith('--import')) {
+    const [arg] = args.splice(i, 1)
+    let [, path] = arg.split('=')
+    if (!path) {
+      path = args.splice(i, 1)[0]
+    }
+    await import(path)
+    --i
+  }
+}
+
+await start({ name: 'yakumo' })
