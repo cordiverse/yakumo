4a9c97b437e49e4bcb31049b3843f9b08a7d567f
Shigma <shigma10826@gmail.com>
2025-02-25 01:20:35 +0800

    feat(esbuild): support --minify, close #24

--- packages/esbuild/src/index.ts
+++ packages/esbuild/src/index.ts
@@ -1,17 +1,30 @@
 import { Context } from 'yakumo'
 import { load } from 'tsconfig-utils'
 import dumble from 'dumble'
+import z from 'schemastery'
 
 export const inject = ['yakumo']
 
-export function apply(ctx: Context) {
+export interface Config {
+  minify: boolean
+}
+
+export const Config: z<Config> = z.object({
+  minify: z.boolean(),
+})
+
+export function apply(ctx: Context, config: Config) {
   ctx.register('esbuild', async () => {
     const paths = ctx.yakumo.locate(ctx.yakumo.argv._)
     await Promise.all(paths.map(async (path) => {
       const cwd = ctx.yakumo.cwd + path
       const tsconfig = await load(cwd).catch(() => null)
       if (!tsconfig) return
-      await dumble(cwd, ctx.yakumo.workspaces[path], tsconfig)
+      await dumble(cwd, ctx.yakumo.workspaces[path], tsconfig, {
+        minify: ctx.yakumo.argv.minify ?? config.minify,
+      })
     }))
+  }, {
+    boolean: ['minify'],
   })
 }
